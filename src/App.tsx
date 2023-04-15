b8c4efe575a636f10b7d27b39d7b86e4f585165e
Jett Thompson <jett.thompson.59@gmail.com>
2023-04-15 20:17:24 +1000

    add the dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { CssBaseline, ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
+import React, { useMemo } from 'react';
 
 export default function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = useMemo(() => createTheme({
+    palette: {
+      mode: prefersDarkMode ? 'dark' : 'light',
+    },
+  }), [prefersDarkMode]);
   return (
-    <h1>It works!</h1>
-      );
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <h1>Hello</h1>
+    </ThemeProvider>
+  );
 }
